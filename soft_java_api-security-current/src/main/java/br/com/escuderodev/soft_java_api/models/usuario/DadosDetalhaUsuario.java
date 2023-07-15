cb18e7595cf30cea21f06724c11ac4e9a49ac569
escuderodev <escudro.ti@gmail.com>
2023-07-15 16:48:40 -0300

    att

--- soft_java_api-security-current/src/main/java/br/com/escuderodev/soft_java_api/models/usuario/DadosDetalhaUsuario.java
+++
@@ -1,8 +0,0 @@
-package br.com.escuderodev.soft_java_api.models.usuario;
-
-public record DadosDetalhaUsuario(Long idusuario, String nome, String username, String password) {
-
-    public DadosDetalhaUsuario(Usuario usuario) {
-        this(usuario.getIdusuario(), usuario.getNome(), usuario.getUsername(), usuario.getPassword());
-    }
-}
