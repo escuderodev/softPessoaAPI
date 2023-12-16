e1a17a96edbf73f0399fcf951aa2a20e4eec6f5f
escuderodev <escudero.ti@gmail.com>
2023-12-16 10:14:31 -0300

    commit para troca ded máquina

--- soft_java_api/src/main/java/br/com/escuderodev/soft_java_api/SoftJavaApiApplication.java
+++ soft_java_api/src/main/java/br/com/escuderodev/soft_java_api/SoftJavaApiApplication.java
@@ -2,8 +2,6 @@ package br.com.escuderodev.soft_java_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SoftJavaApiApplication {
